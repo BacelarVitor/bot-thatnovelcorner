bf030e38e609551555ceb0c8d9c8ac627cff1d2f
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-12-28 21:05:40 -0300

    fon

---
+++ Novel.cs
@@ -0,0 +1,10 @@
+public class Novel
+{
+    public int Volume { get; set; }
+    public string AdlyLink { get; set; }
+    public string MegaLink { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string FileName { get; set; }
+    
+}
